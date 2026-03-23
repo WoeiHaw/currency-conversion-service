c991faebe1f32a891c402997ac6bf03b35fc3378
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-23 11:11:00 +0800

    Add code

---
+++ src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyConversion.java
@@ -0,0 +1,83 @@
+package com.in28minutes.microservices.currency_conversion_service;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+
+    private Long id;
+    private String fromm;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+    public CurrencyConversion() {
+    }
+
+    public CurrencyConversion(Long id, String fromm, String to, BigDecimal quantity,BigDecimal conversionMultiple,  BigDecimal totalCalculatedAmount, String environment) {
+        this.id = id;
+        this.fromm = fromm;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFromm() {
+        return fromm;
+    }
+
+    public void setFromm(String fromm) {
+        this.fromm = fromm;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return totalCalculatedAmount;
+    }
+
+    public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+        this.totalCalculatedAmount = totalCalculatedAmount;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
--- src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -1,4 +1,59 @@
 package com.in28minutes.microservices.currency_conversion_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyCOnversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+            ){
+
+        HashMap<String,String> uriVariable = new HashMap<>();
+        uriVariable.put("from",from);
+        uriVariable.put("to",to);
+        ResponseEntity<CurrencyConversion> currencyConversionResponseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,uriVariable);
+        CurrencyConversion currencyConversion = currencyConversionResponseEntity.getBody();
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + "rest template");
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyCOnversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ){
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from,to);
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()+ " " + "feign");
+    }
 }
+
